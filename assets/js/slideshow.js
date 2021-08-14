165f51a2593985e0fae7cc994b1b84cf8ee7e5f7
Red-Essence <44193134+Red-Essence@users.noreply.github.com>
2021-08-14 03:23:43 +0100

    Moved arrow buttons

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -2,6 +2,8 @@
 var slideIndex = 0;
 var mySlides;
 var textContent;
+var slideButtons;
+var lightButtons = false;
 
 function moveDiv(n){
     showDivs(((slideIndex + n + mySlides.length) % (mySlides.length)));
@@ -17,11 +19,26 @@ function showDivs(n){
         var clone = mySlides[n].children[x].cloneNode(true);
         textContent.appendChild(clone);
     }
+    if (!lightButtons && mySlides[n].classList.contains("lightImage")){
+        for (var x = 0; x < slideButtons.length; x++){
+            slideButtons[x].classList.add("lightButton");
+            slideButtons[x].classList.add("lightButton");
+        }
+        lightButtons = true;
+    }
+    else if (lightButtons && !mySlides[n].classList.contains("lightImage")){
+        for (var x = 0; x < slideButtons.length; x++){
+            slideButtons[x].classList.remove("lightButton");
+            slideButtons[x].classList.remove("lightButton");
+        }
+        lightButtons = false;
+    }
     slideIndex=n;
 }
 
 window.addEventListener('load', function () {
     mySlides = $("div.mySlides");
     textContent = this.document.getElementsByClassName("text-content")[0];
+    slideButtons = this.document.getElementsByClassName("slide-button");
     moveDiv(slideIndex);
 })
